4207206b98b56ee57922bb42a9676bc897dd7427
f0ll0wthewhiterabbit <3tonishere@gmail.com>
2024-10-24 22:12:10 +0300

    Added icons and images

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+// TODO:
+</script>
+
+<template>
+  <header class="header">
+    <nav>
+      <ul class="header__list">
+        <li>
+          <a class="header__link" href="#">Store</a>
+        </li>
+        <li>
+          <a class="header__link" href="#">About</a>
+        </li>
+        <li>
+          <a class="header__link" href="#">Shipping</a>
+        </li>
+        <li>
+          <a class="header__link" href="#">Recipes</a>
+        </li>
+        <li>
+          <a class="header__link" href="#">Blog</a>
+        </li>
+      </ul>
+    </nav>
+    <button></button>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  height: 76px;
+  padding: 0 96px 0 272px;
+}
+</style>
---
+++ src/components/icons/ArrowSvg.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 27 14"
+    aria-hidden="true"
+    focusable="false"
+    fill="none"
+  >
+    <g stroke="currentCOlor" stroke-miterlimit="10" stroke-width="2">
+      <path d="M0 7h24M18 15s2.939-8 9-8c-6.061 0-9-8-9-8" />
+    </g>
+  </svg>
+</template>
