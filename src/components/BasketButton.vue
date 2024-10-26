d31542159348b1e410102a66f96f2d92deb514be
f0ll0wthewhiterabbit <3tonishere@gmail.com>
2024-10-26 19:41:07 +0300

    Added decoration for Order and Basket buttons

--- src/components/BasketButton.vue
+++ src/components/BasketButton.vue
@@ -24,10 +24,92 @@ const itemsInBasket = ref<number>(8)
   position: relative;
   cursor: pointer;
 
+  &::after {
+    content: '';
+    display: block;
+    position: absolute;
+    top: -2px;
+    left: -2px;
+    background-color: #fff;
+    width: 48px;
+    height: 48px;
+    clip-path: polygon(
+      0px 0px,
+      4px 0px,
+      4px 2px,
+      11px 2px,
+      11px 0px,
+      15px 0px,
+      15px 2px,
+      22px 2px,
+      22px 0px,
+      26px 0px,
+      26px 2px,
+      33px 2px,
+      33px 0px,
+      37px 0px,
+      37px 2px,
+      44px 2px,
+      44px 0px,
+      48px 0px,
+      48px 4px,
+      46px 4px,
+      46px 11px,
+      48px 11px,
+      48px 15px,
+      46px 15px,
+      46px 22px,
+      48px 22px,
+      48px 26px,
+      46px 26px,
+      46px 33px,
+      48px 33px,
+      48px 37px,
+      46px 37px,
+      46px 44px,
+      48px 44px,
+      48px 48px,
+      44px 48px,
+      44px 46px,
+      37px 46px,
+      37px 48px,
+      33px 48px,
+      33px 46px,
+      26px 46px,
+      26px 48px,
+      22px 48px,
+      22px 46px,
+      15px 46px,
+      15px 48px,
+      11px 48px,
+      11px 46px,
+      4px 46px,
+      4px 48px,
+      0px 48px,
+      0px 44px,
+      2px 44px,
+      2px 37px,
+      0px 37px,
+      0px 33px,
+      2px 33px,
+      2px 26px,
+      0px 26px,
+      0px 22px,
+      2px 22px,
+      2px 15px,
+      0px 15px,
+      0px 11px,
+      2px 11px,
+      2px 4px,
+      0px 4px
+    );
+  }
+
   &__icon {
     color: #231f20;
     width: 20px;
     height: 20px;
+    z-index: 5;
   }
 
   &__counter {
@@ -48,6 +130,7 @@ const itemsInBasket = ref<number>(8)
     display: flex;
     align-items: center;
     justify-content: center;
+    z-index: 10;
   }
 }
 </style>
